4fab3568a689d9818da3a5159930bcec68f56a0d
Abderrahmane Bouchama <abdou16bm@yahoo.fr>
2023-04-27 09:37:22 +0100

    add detail and delete

--- djangoApp1/urls.py
+++ djangoApp1/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('product/list',views.productList),
     path('product/add/simple',views.productAddSimple),
+    path('product/<int:id>/', views.productDetail),
+    path('product/<int:id>/delete', views.productDelete),
 ]
